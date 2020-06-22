9062ffe3c3b3e760573a8770116aa1d404d98edb
Daniel Gstoehl <gstoehl@ubique.ch>
2020-06-22 15:40:50 +0200

    build flags

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let targets: [Target] = [
 #else
 let targets: [Target] = [
     .systemLibrary(name: "CZLib", pkgConfig: "zlib", providers: [.brew(["zlib"]), .apt(["zlib"])]),
-    .target(name: "ZIPFoundation", dependencies: ["CZLib"], cSettings: [.define("_GNU_SOURCE", to: "1")]),
+    .target(name: "ZIPFoundation", dependencies: ["CZLib"], cSettings: [.define("_GNU_SOURCE", to: "1")], swiftSettings: [.unsafeFlags(["-gnone"])]),
     .testTarget(name: "ZIPFoundationTests", dependencies: ["ZIPFoundation"])
 ]
 #endif
